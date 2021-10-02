9b35d91853b48c7d5009c2ecdaf8b4f56fbf8885
21grahams <graham.kirsh@gmail.com>
2021-10-01 18:34:23 -0600

    added a few files in pages and a dynamically rendered one inside of news folder

---
+++ second-next/pages/news/[important].js
@@ -0,0 +1,12 @@
+// my-domain.com/news/important
+
+
+const Important = () => {
+  return (
+    <h1>
+    The Important Subsection of News Page
+    </h1>
+  )
+}
+
+export default Important;
---
+++ second-next/pages/news/index.js
@@ -0,0 +1,11 @@
+// my-domain.com/news since this is a nested index (home) path inside of news folder inside of pages folder
+
+const NewsPage = () => {
+  return (
+    <h1>
+    The News Home Page
+    </h1>
+  )
+}
+
+export default NewsPage;
